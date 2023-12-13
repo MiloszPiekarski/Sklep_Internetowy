38e6f067a353672754df3b09068af194a305588f
Cincior <krzycho75000@gmail.com>
2023-12-13 17:17:02 +0100

    Dodanie klasy Produkt

---
+++ Produkt.cpp
@@ -0,0 +1,11 @@
+//
+// Created by kkosz on 13.12.2023.
+//
+using namespace std;
+#include "Produkt.h"
+#include <iostream>
+
+Produkt::Produkt()
+{
+    cout << "Utworzono obiekt Produkt!" << endl;
+}
---
+++ Produkt.h
@@ -0,0 +1,21 @@
+//
+// Created by kkosz on 13.12.2023.
+//
+
+#ifndef PROJEKT_PRODUKT_H
+#define PROJEKT_PRODUKT_H
+
+
+class Produkt {
+    int Id_Produktu;
+    int Id_Kategorii;
+    string Nazwa;
+    float Cena;
+    int Dostepnosc;
+
+public:
+    Produkt();
+};
+
+
+#endif //PROJEKT_PRODUKT_H
